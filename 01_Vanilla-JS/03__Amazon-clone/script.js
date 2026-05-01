b551fcb8b9ce392137ecba220f2a17b8c5531b16
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-05-01 12:27:43 +0500

    add image slider functionality with navigation controls and horizontal scroll support

---
+++ 01_Vanilla-JS/03__Amazon-clone/script.js
@@ -0,0 +1,38 @@
+const imgs = document.querySelectorAll(".header-slider ul img");
+ const prev_btn = document.querySelector(".control_prev"); 
+ const next_btn = document.querySelector(".control_next");
+ const scrollContainer = document.querySelectorAll(".products");
+
+let n = 0;
+
+function changeSlide() { 
+    for (let i = 0; i < imgs.length; i++) { 
+        imgs[i].style.display = "none"; 
+        } 
+    imgs[n].style.display = "block"; 
+} 
+    changeSlide();
+
+prev_btn.addEventListener("click", ()=> {
+  if (n > 0){
+      n--; 
+    } else {
+          n = imgs.length - 1;
+     } 
+     changeSlide(); 
+      });
+      next_btn.addEventListener("click", ()=> {
+        if (n < imgs.length - 1){
+            n++; 
+          } else {
+                n = 0;
+           } 
+           changeSlide(); 
+            });
+
+            for (const item of scrollContainer) {
+              item.addEventListener('wheel', (e) => {
+                    e.preventDefault();
+                    item.scrollLeft += e.deltaY;
+              })
+            }
