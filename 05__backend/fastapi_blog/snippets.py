27fc564c090f3f5a4e943d137ee95a94c67ece95
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-05-10 10:16:27 +0500

    Add snippets.py file with sample blog posts data

---
+++ 05__backend/fastapi_blog/snippets.py
@@ -0,0 +1,16 @@
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "Python Tutorial: Python for Beginners",
+        "content": "This is a tutorial for Python beginners.",
+        "date_posted": "April 20, 2018"
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "FastAPI Tutorial: FastAPI for Beginners",
+        "content": "This is a tutorial for FastAPI beginners.",
+        "date_posted": "June 15, 2020"
+    }
+]
