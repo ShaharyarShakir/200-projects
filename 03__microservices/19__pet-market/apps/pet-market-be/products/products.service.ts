8a075cc45e33049d52e435000c3bef631d2334c3
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-05-10 10:11:42 +0500

    Add ProductsService with CRUD operations for Product entity

---
+++ 03__microservices/19__pet-market/apps/pet-market-be/products/products.service.ts
@@ -0,0 +1,33 @@
+import { PrismaService } from './../src/app/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateProductInput } from './dto/create-product.input';
+import { UpdateProductInput } from './dto/update-product.input';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) { }
+  create(createProductInput: CreateProductInput) {
+
+    return 'This action adds a new product';
+  }
+
+  findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.product.findFirst({
+      where:{
+        id
+      }
+    });
+  }
+
+  update(id: number, updateProductInput: UpdateProductInput) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
