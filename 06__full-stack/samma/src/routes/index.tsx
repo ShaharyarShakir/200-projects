fface32db49e59deb1525103516efdf2054999cc
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-05-13 08:22:20 +0500

    Add index route: implement Home component with welcome message and switch

---
+++ 06__full-stack/samma/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Switch } from '#/components/ui/switch'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({ component: Home })
+
+function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="font-bold text-4xl">Welcome to TanStack Start</h1>
+      <p className="mt-4 text-lg">
+        Edit <code>src/routes/index.tsx</code> to get started.
+      </p>
+      <Switch />
+    </div>
+  )
+}
