baa504c7ad2b64cea3f302f2d15d6ac94d9839a5
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-05-29 11:42:08 +0500

    Add layout component: create +layout.svelte to define the main application structure, including header, footer, and dynamic meta tags, enhancing SEO and user experience in the SvelteKit application.

---
+++ 06__full-stack/shakir-folio/src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '../app.css';
+	import '@fontsource-variable/urbanist';
+	import { PrismicPreview } from '@prismicio/svelte/kit';
+	import { page } from '$app/state';
+	import { repositoryName } from '$lib/prismicio';
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+	let { children, data } = $props();
+</script>
+
+<svelte:head>
+	<title>{page.data.title}</title>
+	{#if page.data.meta_description}
+		<meta name="description" content={page.data.meta_description} />
+	{/if}
+	{#if page.data.meta_title}
+		<meta name="og:title" content={page.data.meta_title} />
+	{/if}
+	{#if page.data.meta_image}
+		<meta name="og:image" content={page.data.meta_image} />
+		<meta name="twitter:card" content="summary_large_image" />
+	{/if}
+</svelte:head>
+<Header settings={data.settings} />
+<main>
+	{@render children()}
+</main>
+<Footer settings={data.settings} />
+<PrismicPreview {repositoryName} />
