d8c69471af3a185ca06111e528a88c3c7303b2b4
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-07 12:09:58 +0500

    Add Todo API functionality: implement GET and POST endpoints for managing todos, including error handling for empty todo body and response formatting.

--- 06__full-stack/todo_app/backend/main.go
+++ 06__full-stack/todo_app/backend/main.go
@@ -1,12 +1,37 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
 
 func main() {
+	fmt.Println("Hello Golang")
 	app := fiber.New()
-
+	todos := []Todo{}
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World! Shakir")
+		return c.Status(200).JSON(fiber.Map{"message": "Hello World"})
+	})
+
+	app.Post("/api/todos", func(c fiber.Ctx) error {
+		todo := &Todo{}
+		if err := c.Bind().Body(todo); err != nil {
+			return err
+		}
+		if todo.Body == "" {
+			c.Status(400).JSON(fiber.Map{"error": "Todo body not found"})
+		}
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
 	})
 
 	app.Listen(":3000")
