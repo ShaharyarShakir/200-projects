8b34cb4a4ba5a4a383a0e8918b1abbf75c884aca
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-08 08:58:37 +0500

    Add main entry point: create main.tsx to render the application with React, integrating TooltipProvider and Toaster for enhanced user interface interactions.

---
+++ 04__ai/document_copilot/frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { TooltipProvider } from './components/ui/tooltip.tsx'
+import { Toaster } from './components/ui/sonner.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+   <TooltipProvider>
+      <App />
+      <Toaster position="top-center" richColors />
+    </TooltipProvider>
+  </StrictMode>,
+)
