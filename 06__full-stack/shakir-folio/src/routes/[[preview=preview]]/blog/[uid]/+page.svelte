591ac685786c576a8baaaebc09f9f20cf8fc0ec5
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-08 17:46:27 +0500

    Add +page.svelte for blog post detail view, incorporating ContentBody component to render page content based on dynamic data.

---
+++ 06__full-stack/shakir-folio/src/routes/[[preview=preview]]/blog/[uid]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import ContentBody from '$lib/components/ContentBody.svelte';
+	import type { PageProps } from './$types';
+
+	const { data }: PageProps = $props();
+</script>
+
+<ContentBody page={data.page} />
