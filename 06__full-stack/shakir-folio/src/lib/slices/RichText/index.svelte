a5117fbc66e3f0c8731901a1b8bbceb2fea144b1
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-12 10:03:19 +0500

    Add RichText slice with initial implementation and mock data

---
+++ 06__full-stack/shakir-folio/src/lib/slices/RichText/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+	import { PrismicRichText, type SliceComponentProps } from '@prismicio/svelte';
+
+	type Props = SliceComponentProps<Content.RichTextSlice>;
+
+	const { slice }: Props = $props();
+</script>
+
+<section class="max-w-prose">
+	<PrismicRichText field={slice.primary.content} />
+</section>
