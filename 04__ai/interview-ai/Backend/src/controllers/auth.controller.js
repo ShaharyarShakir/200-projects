9bfe13ca7fc6fae978301591cd1585824c954452
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-12 23:19:11 +0500

    feat: add registerUserController for user registration

---
+++ 04__ai/interview-ai/Backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+
+
+
+/**
+ * @name registerUserController
+ * @description register a new user , email and password in the request body
+ * @access Public
+ */
+
+
+
+export async function registerUserController(req, res) {
+    
+}
+
