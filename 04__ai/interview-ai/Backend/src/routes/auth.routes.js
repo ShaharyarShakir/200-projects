68ebcc691ef2261dcf88eca0a245fa18fee28728
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-12 23:31:53 +0500

    feat: create auth router for user registration endpoint

---
+++ 04__ai/interview-ai/Backend/src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+import {Router} from "express"
+
+const authRouter = Router()
+
+/**
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+
+// authRouter.post('/register')
+
+export default authRouter
