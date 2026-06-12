ff8b2512e019812eef24bdaeb9ac07f30ed50a9a
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-12 22:09:39 +0500

    feat: add root route and document structure for ShopStack application

---
+++ 06__full-stack/shop-stack/src/routes/__root.tsx
@@ -0,0 +1,61 @@
+import { HeadContent, Scripts, createRootRouteWithContext } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import appCss from '../styles.css?url'
+import type { QueryClient } from '@tanstack/react-query'
+import { ThemeProvider } from '#/components/base/provider/theme-provider'
+import { Toaster } from 'sonner'
+
+
+export const Route = createRootRouteWithContext<{queryClient: QueryClient}>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'ShopStack Multi-Tenet E-com Store',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+  shellComponent: RootDocument,
+})
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        {children}
+        <Toaster richColors closeButton  position='top-right' />
+        </ThemeProvider>
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
