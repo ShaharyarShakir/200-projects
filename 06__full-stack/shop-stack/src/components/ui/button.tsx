02957f2c2c621617f194cb20ea58eb11e9ce394b
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-13 11:33:17 +0500

    chore: fix formatting in buttonVariants for consistency

--- 06__full-stack/shop-stack/src/components/ui/button.tsx
+++ 06__full-stack/shop-stack/src/components/ui/button.tsx
@@ -14,8 +14,7 @@ const buttonVariants = cva(
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "border-2 bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
           "border-2 border-dashed text-body-70 hover:border-transparent hover:bg-accent hover:text-accent-foreground dark:hover:bg-primary",
         link: "text-primary underline-offset-4 hover:underline",
@@ -34,7 +33,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
+  },
 );
 
 function Button({
