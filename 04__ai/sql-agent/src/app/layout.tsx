2f948570ebd60f125c98a516884f9518afb4faa4
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-13 09:54:51 +0500

    feat: add RootLayout component and metadata for SQL agent

---
+++ 04__ai/sql-agent/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sql Agent",
+  description: "An AI agent that queries the database for the common people using natural language",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="flex flex-col min-h-full">{children}</body>
+    </html>
+  );
+}
