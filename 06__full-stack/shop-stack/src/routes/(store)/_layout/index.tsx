fbea1a4c995ae3246e051e48cb6231d21e7a2815
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-13 11:34:50 +0500

    fix: standardize code formatting in index.tsx

--- 06__full-stack/shop-stack/src/routes/(store)/_layout/index.tsx
+++ 06__full-stack/shop-stack/src/routes/(store)/_layout/index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/(store)/_layout/')({
+export const Route = createFileRoute("/(store)/_layout/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello</div>
+  return <div>Hello</div>;
 }
