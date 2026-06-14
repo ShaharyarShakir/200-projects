c29cdec1d2059b6180a9a10d6763afe8ae477ec0
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-14 12:00:33 +0500

    feat: add configuration file for SvelteKit blog with title, description, and dynamic URL

---
+++ 06__full-stack/smorte_blog/src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'smort'
+export const description = 'SvelteKit blog for developers'
+export const url = dev ? 'http://localhost:5173' : 'https://smorte.vercel.app'
