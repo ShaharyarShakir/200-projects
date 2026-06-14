aa1ffa9a8517f4f8cb2658f03adfdc8a0154023c
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-14 14:50:19 +0500

    feat: add config.js for environment variable management

---
+++ 06__full-stack/sockploy/src/config.js
@@ -0,0 +1,3 @@
+export const MANAGEMENT_PORT = process.env.MANAGEMENT_PORT ?? 8080;
+export const RESOLVE_REVERSE_PROXY = process.env.RESOLVE_REVERSE_PROXY ?? 'localhost';
+export const NETWORK_NAME = process.env.NETWORK_NAME ?? 'sockploy-network';
