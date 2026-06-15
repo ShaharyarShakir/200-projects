a7cd0309dc7cdc83da7959322808891508241e70
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-15 21:23:57 +0500

    style: improve code formatting for consistency in product page components and remove unused product validation file

--- 06__full-stack/ibuildthis/app/proudct/page.tsx
+++ 06__full-stack/ibuildthis/app/proudct/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Product() {
-    return (
-        <div>
-            <h1>Product</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  );
 }
