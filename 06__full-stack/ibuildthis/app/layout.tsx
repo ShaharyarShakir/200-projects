38eb849d732d3af1cef545a19fcff50e7f76e2b6
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-15 18:49:55 +0500

    feat: add RootLayout component with metadata and layout structure

---
+++ 06__full-stack/ibuildthis/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Outfit, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const geistSans = Outfit({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "IBuildThis - Share your creations, Discover  New Launches",
+  description: "A community platform for creators to showcase their apps, AI tools, SaaS products and creative projects. Authentic launches, real builders, genuine feedback.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ClerkProvider>
+          <Header />
+          {children}
+          <Footer />
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
