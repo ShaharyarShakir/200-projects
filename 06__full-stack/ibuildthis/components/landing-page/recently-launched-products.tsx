9aef785e1cfc33bf92e7f29b86f28d7efa34bbd7
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-15 21:24:34 +0500

    style: improve code formatting for consistency in RecentlyLaunchedProducts and StatsCard components

--- 06__full-stack/ibuildthis/components/landing-page/recently-launched-products.tsx
+++ 06__full-stack/ibuildthis/components/landing-page/recently-launched-products.tsx
@@ -1,9 +1,9 @@
-import SectionHeader from '@/components/common/section-header'
-import { CalendarIcon, RocketIcon } from 'lucide-react'
+import SectionHeader from "@/components/common/section-header";
+import { CalendarIcon, RocketIcon } from "lucide-react";
 
-import ProductCard from '@/components/products/product-card'
-import EmptyState from '@/components/common/empyt-state';
-import { getRecentlyLaunchedProducts } from '@/lib/products/product-select';
+import ProductCard from "@/components/products/product-card";
+import EmptyState from "@/components/common/empyt-state";
+import { getRecentlyLaunchedProducts } from "@/lib/products/product-select";
 
 // const recentlyLaucnhedProducts = [
 //     {
@@ -25,26 +25,28 @@ import { getRecentlyLaunchedProducts } from '@/lib/products/product-select';
 
 // ];
 export default async function RecentlyLaunchedProducts() {
-    const recentlyLaucnhedProducts = await getRecentlyLaunchedProducts()
-    return (
-        <section className='py-20'>
-            <div className='wrapper'>
-                <SectionHeader
-                    title='Recently Launched'
-                    icon={RocketIcon}
-                    description='The most recently launced products from our community'
-
-                />
-                {recentlyLaucnhedProducts.length > 0 ? (
-                    <div className="grid-wrapper">
-                        {recentlyLaucnhedProducts.map((product) => (
-                            <ProductCard key={product.id} product={product} />
-                        ))}
-                    </div>
-                ) : (
-                    <EmptyState message="No Product launch in the last week. Check back soon!!" icon={CalendarIcon} />
-                )}
-            </div>
-        </section>
-    )
+  const recentlyLaucnhedProducts = await getRecentlyLaunchedProducts();
+  return (
+    <section className="py-20">
+      <div className="wrapper">
+        <SectionHeader
+          title="Recently Launched"
+          icon={RocketIcon}
+          description="The most recently launced products from our community"
+        />
+        {recentlyLaucnhedProducts.length > 0 ? (
+          <div className="grid-wrapper">
+            {recentlyLaucnhedProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        ) : (
+          <EmptyState
+            message="No Product launch in the last week. Check back soon!!"
+            icon={CalendarIcon}
+          />
+        )}
+      </div>
+    </section>
+  );
 }
--- 06__full-stack/ibuildthis/components/landing-page/stats-card.tsx
+++ 06__full-stack/ibuildthis/components/landing-page/stats-card.tsx
@@ -1,25 +1,25 @@
-import { cn } from '@/lib/utils'
-import { LucideIcon } from 'lucide-react'
-import React from 'react'
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+import React from "react";
 
 export default function StatsCard({
-    icon: Icon,
-    value,
-    label,
-    hasBorder
+  icon: Icon,
+  value,
+  label,
+  hasBorder,
 }: {
-    icon: LucideIcon,
-    value: string,
-    label: string,
-    hasBorder?: boolean
+  icon: LucideIcon;
+  value: string;
+  label: string;
+  hasBorder?: boolean;
 }) {
-    return (
-        <div className={cn("space-y-2", hasBorder && "border-x border-border/50")}>
-            <div className='flex justify-center items-center gap-2'>
-                <Icon className='size-5 text-primary/70' />
-                <p className='font-bold text-3xl sm:text-4xl' > {value} </p>
-            </div>
-            <p className='text-muted-foreground text-sm'>{label} </p>
-        </div>
-    )
+  return (
+    <div className={cn("space-y-2", hasBorder && "border-x border-border/50")}>
+      <div className="flex justify-center items-center gap-2">
+        <Icon className="size-5 text-primary/70" />
+        <p className="font-bold text-3xl sm:text-4xl"> {value} </p>
+      </div>
+      <p className="text-muted-foreground text-sm">{label} </p>
+    </div>
+  );
 }
