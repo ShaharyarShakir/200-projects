729e76bc34baa6aadc0e2cd8ff4d19142f50f2d8
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-16 14:13:35 +0500

    feat: integrate @drop-in/graffiti and remove unused layout.css

--- 06__full-stack/remote_blog/src/routes/+layout.svelte
+++ 06__full-stack/remote_blog/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import './layout.css';
+	import '@drop-in/graffiti';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<main class="layout-readable center">{@render children()}</main>
