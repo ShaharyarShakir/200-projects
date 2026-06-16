07740838f8db15b0a7bdf9bed1f39eb8610e12e7
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-16 15:30:25 +0500

    feat: enhance ProductSubmitForm to display messages with conditional styling

--- 06__full-stack/ibuildthis/components/products/product-submit-form.tsx
+++ 06__full-stack/ibuildthis/components/products/product-submit-form.tsx
@@ -7,6 +7,7 @@ import { LoaderIcon, SparkleIcon } from "lucide-react";
 
 import { useActionState } from "react";
 import { addProductAction } from "./product-action";
+import { cn } from "@/lib/utils";
 
 const initialState = {
   success: false,
@@ -19,6 +20,9 @@ export default function ProductSubmitForm() {
   const { errors, message, success } = state;
   return (
     <form className="space-y-6" action={formAction}>
+      {message && (
+        <div className={cn("p-4 border rounded-lg", success ? "bg-primary/10 border-primary text-primary" : "bg-destructive/10 border-destructive text-destructive")}> {message} </div>
+      )}
       <div className="space-y-2">
         <FormField
           label="Product Name"
@@ -27,7 +31,7 @@ export default function ProductSubmitForm() {
           placeholder="My Awesome Product"
           required
           onChange={() => {}}
-          error=""
+          error=''
           // error={getFieldErrors("name")}
         />
         <FormField
