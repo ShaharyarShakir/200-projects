a2bd232887f05ae6fb4bb7b0d3d02256564a0804
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-21 00:34:44 +0500

    feat: create shared index file to export types, schemas, constants, and utils

---
+++ 06__full-stack/business-management-suite/packages/shared/index.ts
@@ -0,0 +1,24 @@
+// Types
+export * from './types/user.types';
+export * from './types/employee.types';
+export * from './types/inventory.types';
+export * from './types/customer.types';
+export * from './types/attendance.types';
+export * from './types/notification.types';
+export * from './types/report.types';
+
+// Schemas
+export * from './schemas/auth.schema';
+export * from './schemas/employee.schema';
+export * from './schemas/inventory.schema';
+export * from './schemas/customer.schema';
+
+// Constants
+export * from './constants/roles';
+export * from './constants/permissions';
+export * from './constants/api-routes';
+
+// Utils
+export * from './utils/api-client';
+export * from './utils/date-utils';
+export * from './utils/format-utils';
