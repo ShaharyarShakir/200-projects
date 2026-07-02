303ef14e95227d565766e5b8ab8c72f39c5ca7d5
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-02 12:28:35 +0500

    fix: correct formatting and ensure consistent middleware configuration

--- 04__ai/build-your-own-adventure/backend/main.py
+++ 04__ai/build-your-own-adventure/backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from core.config import settings
+
 app = FastAPI(
     title="Choose your own adventure game",
     description="AI api to generate cool stories",
@@ -14,11 +15,11 @@ app.add_middleware(
     allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
 
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
