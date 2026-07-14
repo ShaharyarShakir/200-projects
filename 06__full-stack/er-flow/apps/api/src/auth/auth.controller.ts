11f185caaefa1481130f1456ed05a8cd0755a8e8
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-14 10:59:16 +0500

    feat: add AuthController to handle authentication requests

---
+++ 06__full-stack/er-flow/apps/api/src/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, All, Req, Res } from "@nestjs/common";
+import * as express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./auth";
+
+@Controller("api/auth")
+export class AuthController {
+  @All("*")
+  async handleAuth(@Req() req: express.Request, @Res() res: express.Response) {
+    return toNodeHandler(auth)(req, res);
+  }
+}
