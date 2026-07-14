869e3b41adbee812cd152096b1c7e01d610bcf5c
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-14 16:14:53 +0500

    feat: add index file to export API modules for client, auth, workspace, document, whiteboard, user, and health

---
+++ 06__full-stack/er-flow/packages/api-client/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./client";
+export * from "./auth";
+export * from "./workspace";
+export * from "./document";
+export * from "./whiteboard";
+export * from "./user";
+export * from "./health";
