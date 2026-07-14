444fa1e95e2e084f2c9c0af2fbe67ed8e1fe26a7
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-14 12:57:26 +0500

    feat: add favicon.ico and Vite configuration for web app setup

---
+++ 06__full-stack/er-flow/apps/web/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  build: {
+    rolldownOptions: {
+      external: ['env', 'wasi_snapshot_preview1']
+    }
+  },
+  plugins: [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+})
+
+export default config
