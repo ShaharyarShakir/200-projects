d031864e7418f0147d2a520a15bc7aad24277622
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-15 14:30:53 +0500

    feat: create index.ts to export core and extension modules for the editor

---
+++ 06__full-stack/er-flow/packages/editor/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./core/editor";
+export * from "./extensions/math";
+export * from "./extensions/mermaid";
+export * from "./extensions/slash-commands";
+export * from "./extensions/code-block";
+export * from "./markdown";
+
