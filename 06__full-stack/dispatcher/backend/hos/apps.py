f4482a703baddfb80ac0358e7370a270c4d09e15
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-22 01:23:34 +0500

    fix: standardize string quotes in apps.py for consistency

--- 06__full-stack/dispatcher/backend/hos/apps.py
+++ 06__full-stack/dispatcher/backend/hos/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class HosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hos'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hos"
