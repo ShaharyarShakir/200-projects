a73156831ac99226e54f55b8f7ebd6a0a9438268
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-22 01:33:33 +0500

    style: update MapPreview component to use dark tile layer for improved map aesthetics

--- 06__full-stack/dispatcher/frontend/src/features/location/components/MapPreview.tsx
+++ 06__full-stack/dispatcher/frontend/src/features/location/components/MapPreview.tsx
@@ -65,7 +65,7 @@ export const MapPreview: React.FC<MapPreviewProps> = ({
           <ChangeView center={center} zoom={zoom} />
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}{r}.png"
           />
           <Marker position={center}>
             {address && (
