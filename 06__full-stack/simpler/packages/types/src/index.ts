86551ab499f8a01cab1f5bb6253bb68d266d37cc
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-23 13:48:01 +0500

    feat: define User, Task, and Note interfaces in types package

---
+++ 06__full-stack/simpler/packages/types/src/index.ts
@@ -0,0 +1,14 @@
+export interface User {
+  id: string;
+  email: string;
+}
+
+export interface Task {
+  id: string;
+  title: string;
+}
+
+export interface Note {
+  id: string;
+  title: string;
+}
