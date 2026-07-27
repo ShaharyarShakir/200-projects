641bfc5c0ed71aaefca1406624480eff9cc2a641
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-28 02:06:41 +0500

    feat: add utility functions and types for class name management and type manipulation

---
+++ 06__full-stack/github-dashboard/src/lib/utils.ts
@@ -0,0 +1,13 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
+
+export type WithElementRef<T> = T & {
+	ref?: any;
+};
+
+export type WithoutChildrenOrChild<T> = Omit<T, "children" | "child">;
+export type WithoutChild<T> = Omit<T, "child">;
