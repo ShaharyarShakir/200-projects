6e47bfeec1535c0e7a536b79f5e2ebe792a143da
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-30 15:39:13 +0500

    feat: update ESLint rules and modify db:start script for Docker

--- 06__full-stack/github-dashboard/eslint.config.js
+++ 06__full-stack/github-dashboard/eslint.config.js
@@ -34,8 +34,10 @@ export default defineConfig(
 		}
 	},
 	{
-		// Override or add rule settings here, such as:
-		// 'svelte/button-has-type': 'error'
-		rules: {}
+		rules: {
+			'svelte/no-navigation-without-resolve': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'svelte/no-at-html-tags': 'off'
+		}
 	}
 );
