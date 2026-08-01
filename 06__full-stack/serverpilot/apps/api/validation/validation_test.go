5e92f0f3a1cf2bb8ec980ce1e5f6ed628b9d30cf
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-01 09:22:31 +0500

    feat: update ValidateCreateServerInput tests to include new parameters for server configuration

--- 06__full-stack/serverpilot/apps/api/validation/validation_test.go
+++ 06__full-stack/serverpilot/apps/api/validation/validation_test.go
@@ -77,7 +77,7 @@ func TestValidateCreateServerInput(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := ValidateCreateServerInput(tt.srvName, tt.ip, tt.os, tt.provider, tt.location, tt.tags)
+			got, err := ValidateCreateServerInput(tt.srvName, tt.ip, tt.os, tt.provider, tt.location, tt.tags, 22, "root", "password", "mypassword", "", "")
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ValidateCreateServerInput() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -137,6 +137,6 @@ func BenchmarkValidateCreateServerInput(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _ = ValidateCreateServerInput(name, ip, osStr, provider, loc, tags)
+		_, _ = ValidateCreateServerInput(name, ip, osStr, provider, loc, tags, 22, "root", "password", "mypassword", "", "")
 	}
 }
