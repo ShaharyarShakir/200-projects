71f779276cbb05f84482550ce89c99b38b5bb85f
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-05 20:16:39 +0500

    feat: add request interceptor to include authorization token in API requests

--- 06__full-stack/nimbusdrive/apps/web/src/api/client.js
+++ 06__full-stack/nimbusdrive/apps/web/src/api/client.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../features/auth/authStore";
 
 const client = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
@@ -7,5 +8,18 @@ const client = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+client.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default client;
