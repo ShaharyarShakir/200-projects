4aa133ca1c60951a89dc75dc8f3e6cf348f07f14
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-06 20:31:21 +0500

    style: improve code formatting by removing unnecessary blank lines in driveStore

--- 06__full-stack/nimbusdrive/apps/web/src/features/drive/driveStore.js
+++ 06__full-stack/nimbusdrive/apps/web/src/features/drive/driveStore.js
@@ -3,13 +3,13 @@ import { create } from "zustand";
 export const useDriveStore = create((set) => ({
   activeTab: "all", // "all", "starred", "trash"
   setActiveTab: (tab) => set({ activeTab: tab }),
-  
+
   searchQuery: "",
   setSearchQuery: (query) => set({ searchQuery: query }),
-  
+
   selectedFile: null,
   setSelectedFile: (file) => set({ selectedFile: file }),
-  
+
   previewFile: null,
   setPreviewFile: (file) => set({ previewFile: file }),
 }));
