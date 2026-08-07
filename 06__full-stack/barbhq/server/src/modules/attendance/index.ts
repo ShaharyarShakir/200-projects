951d6ccceb7fa4bc2c21b14b97682b3e33bc529b
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-07 11:05:44 +0500

    feat: create attendance module index file to export components

---
+++ 06__full-stack/barbhq/server/src/modules/attendance/index.ts
@@ -0,0 +1,5 @@
+export * from './attendance.types';
+export * from './attendance.model';
+export * from './attendance.repository';
+export * from './attendance.service';
+export * from './attendance.routes';
