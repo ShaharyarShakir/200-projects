aa37eeb291f21bd3a848fc7bc9a11443bec63efa
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-07 11:26:56 +0500

    feat: create employee module index file to export related components

---
+++ 06__full-stack/barbhq/server/src/modules/employee/index.ts
@@ -0,0 +1,6 @@
+export * from './employee.types';
+export * from './employee.model';
+export * from './employee.repository';
+export * from './employee.service';
+export * from './employee.mapper';
+export * from './employee.routes';
