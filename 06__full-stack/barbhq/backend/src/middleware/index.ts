e95b482f99eab4425464534bfe705eee81c8d383
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-09 12:49:54 +0500

    feat: create middleware index file to export middleware functions and types

---
+++ 06__full-stack/barbhq/backend/src/middleware/index.ts
@@ -0,0 +1,6 @@
+export { errorHandler } from './error.middleware';
+export { notFound } from './notFound.middleware';
+export { validate } from './validate.middleware';
+export type { ValidationSchema } from './validate.middleware';
+export { authenticate, authorize } from './auth.middleware';
+export type { AuthenticatedUser } from './auth.middleware';
