286b671a7c4186dde4f89f7d82ce50e721690939
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-10 09:36:40 +0500

    feat: create index file to export branch module components

---
+++ 06__full-stack/barbhq/backend/src/modules/branch/index.ts
@@ -0,0 +1,6 @@
+export * from './branch.types';
+export * from './branch.repository';
+export * from './branch.service';
+export * from './branch.validator';
+export * from './branch.controller';
+export * from './branch.routes';
