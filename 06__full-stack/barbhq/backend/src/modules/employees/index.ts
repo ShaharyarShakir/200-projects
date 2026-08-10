c4656ac34d62056e0cb236c87931e658930ad075
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-10 12:34:33 +0500

    feat: add employee module exports for better organization

---
+++ 06__full-stack/barbhq/backend/src/modules/employees/index.ts
@@ -0,0 +1,7 @@
+export * from './employee.types';
+export * from './employee.mapper';
+export * from './employee.repository';
+export * from './employee.service';
+export * from './employee.validator';
+export * from './employee.controller';
+export * from './employee.routes';
