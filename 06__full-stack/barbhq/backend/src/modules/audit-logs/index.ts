bd2e2f0f2885138ea0f12c8389bdceafbd30dae7
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-10 09:30:02 +0500

    feat: create index file to export audit log modules

---
+++ 06__full-stack/barbhq/backend/src/modules/audit-logs/index.ts
@@ -0,0 +1,5 @@
+export * from './audit-log.types';
+export * from './audit-log.repository';
+export * from './audit-log.service';
+export * from './audit-log.controller';
+export * from './audit-log.routes';
