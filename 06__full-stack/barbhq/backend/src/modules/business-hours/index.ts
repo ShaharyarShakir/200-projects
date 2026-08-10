dc3169170817a76d4dc90c5ff29a74d23993f58f
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-10 09:38:56 +0500

    feat: create index file for business hours module exports

---
+++ 06__full-stack/barbhq/backend/src/modules/business-hours/index.ts
@@ -0,0 +1,6 @@
+export * from './business-hours.types';
+export * from './business-hours.repository';
+export * from './business-hours.service';
+export * from './business-hours.validator';
+export * from './business-hours.controller';
+export * from './business-hours.routes';
