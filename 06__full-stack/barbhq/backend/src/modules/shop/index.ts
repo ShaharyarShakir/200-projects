0e05389eeb8963b4c4db7693dae3f7afedf20b83
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-11 16:37:53 +0500

    feat: create index file for shop module to export related components

---
+++ 06__full-stack/barbhq/backend/src/modules/shop/index.ts
@@ -0,0 +1,7 @@
+export * from './shop.types';
+export * from './shop.repository';
+export * from './shop.service';
+export * from './shop.validator';
+export * from './shop.mapper';
+export * from './shop.controller';
+export * from './shop.routes';
