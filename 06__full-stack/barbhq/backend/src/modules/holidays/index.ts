d4eb7c3e5c91ef6dc7a53133b1daa0fc69ee92a2
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-11 09:31:16 +0500

    feat: create index file to export holiday module components

---
+++ 06__full-stack/barbhq/backend/src/modules/holidays/index.ts
@@ -0,0 +1,6 @@
+export * from './holiday.types';
+export * from './holiday.repository';
+export * from './holiday.service';
+export * from './holiday.validator';
+export * from './holiday.controller';
+export * from './holiday.routes';
