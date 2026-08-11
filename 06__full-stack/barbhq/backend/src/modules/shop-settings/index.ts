959399a0c71171b06ea2456031878992dc9b117d
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-11 18:17:54 +0500

    feat: create index file for shop-settings module exports

---
+++ 06__full-stack/barbhq/backend/src/modules/shop-settings/index.ts
@@ -0,0 +1,6 @@
+export * from './shop-settings.types';
+export * from './shop-settings.repository';
+export * from './shop-settings.service';
+export * from './shop-settings.validator';
+export * from './shop-settings.controller';
+export * from './shop-settings.routes';
