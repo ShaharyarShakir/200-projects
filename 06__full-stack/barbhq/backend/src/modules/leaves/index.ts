bbd0d591ba3d752d605ddb3c870f64fb3786d031
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-11 16:04:17 +0500

    feat: create index file for leaves module to export related components

---
+++ 06__full-stack/barbhq/backend/src/modules/leaves/index.ts
@@ -0,0 +1,6 @@
+export * from './leave.types';
+export * from './leave.repository';
+export * from './leave.service';
+export * from './leave.validator';
+export * from './leave.controller';
+export * from './leave.routes';
