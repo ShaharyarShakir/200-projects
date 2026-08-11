039735807bb0e8bc918a046a8df7781cd58145bd
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-11 16:13:09 +0500

    feat: add index file for shifts module to export related components

---
+++ 06__full-stack/barbhq/backend/src/modules/shifts/index.ts
@@ -0,0 +1,6 @@
+export * from './shift.types';
+export * from './shift.repository';
+export * from './shift.service';
+export * from './shift.validator';
+export * from './shift.controller';
+export * from './shift.routes';
