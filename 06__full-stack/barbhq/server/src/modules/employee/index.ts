a5fd1471e2c54e76645d95f19921e57a3f2a44ef
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-12 17:53:02 +0500

    refactor: remove employee module and related files

--- 06__full-stack/barbhq/server/src/modules/employee/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './employee.types';
-export * from './employee.model';
-export * from './employee.repository';
-export * from './employee.service';
-export * from './employee.mapper';
-export * from './employee.routes';
--- 06__full-stack/barbhq/server/src/modules/shop/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './shop.types';
-export * from './shop.model';
-export * from './shop.repository';
-export * from './shop.service';
-export * from './shop.routes';
