9500f20e240cbb7b5bda168612ec2bf52968ca40
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-12 09:19:35 +0500

    refactor: remove unused attendance and auth modules

--- 06__full-stack/barbhq/server/src/modules/attendance/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './attendance.types';
-export * from './attendance.model';
-export * from './attendance.repository';
-export * from './attendance.service';
-export * from './attendance.routes';
