cb2ce4c0604d43771c68cd1ead06c13c68be2083
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-12 17:53:13 +0500

    refactor: remove user module and related files

--- 06__full-stack/barbhq/server/src/routes/health.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { getHealth, getVersion } from '../controllers/health.controller';
-
-export const healthRouter = Router();
-
-healthRouter.get('/health', getHealth);
-healthRouter.get('/version', getVersion);
