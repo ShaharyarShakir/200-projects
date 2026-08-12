9ec474e5208ee114d9393da0620a1b37386d386d
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-12 08:40:03 +0500

    feat: add user module exports for types, repository, service, validator, controller, and routes

---
+++ 06__full-stack/barbhq/backend/src/modules/user/index.ts
@@ -0,0 +1,6 @@
+export * from './user.types';
+export * from './user.repository';
+export * from './user.service';
+export * from './user.validator';
+export * from './user.controller';
+export * from './user.routes';
