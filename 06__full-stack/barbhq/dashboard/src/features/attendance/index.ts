00eaf41cb87cad65b043c88ef3a7611260b4ae59
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-16 12:21:22 +0500

    feat: create index file for attendance feature exports

---
+++ 06__full-stack/barbhq/dashboard/src/features/attendance/index.ts
@@ -0,0 +1,5 @@
+export * from "./attendance.types";
+export * from "./attendance.schemas";
+export * from "./attendance.api";
+export * from "./attendance.queries";
+export * from "./attendance.mutations";
