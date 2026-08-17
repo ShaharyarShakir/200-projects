8d0ddcaa6789277de9c9b7d3943cec074f366b24
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-17 14:38:03 +0500

    feat: create index file for auth feature exports

---
+++ 06__full-stack/barbhq/dashboard/src/features/auth/index.ts
@@ -0,0 +1,9 @@
+export * from "./auth.types";
+export * from "./auth.api";
+export * from "./auth-context";
+export * from "./auth-provider";
+export * from "./use-auth";
+export { AuthGuard } from "./components/AuthGuard";
+export { LoginPage } from "./pages/LoginPage";
+export { SignupPage } from "./pages/SignupPage";
+
