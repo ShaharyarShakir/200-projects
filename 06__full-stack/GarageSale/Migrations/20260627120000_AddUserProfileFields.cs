61beaed0e341af935efe2c653a6431b6d2f0bb4e
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-29 20:45:53 +0500

    Add migration to add user profile fields to AspNetUsers table

---
+++ 06__full-stack/GarageSale/Migrations/20260627120000_AddUserProfileFields.cs
@@ -0,0 +1,73 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageSale.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProfileFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address1",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address2",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address3",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogoPath",
+                table: "AspNetUsers",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address1",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Address2",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Address3",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LogoPath",
+                table: "AspNetUsers");
+        }
+    }
+}
