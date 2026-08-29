bf1831833bb5dca6f5fef1830bc019f9c8e5e3ea
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-29 20:44:50 +0500

    Add ConditionRatingDto class to represent condition ratings in the application

---
+++ 06__full-stack/GarageSale/DTOs/ConditionRatingDto.cs
@@ -0,0 +1,8 @@
+namespace GarageSale.DTOs;
+
+public class ConditionRatingDto
+{
+    public int Id { get; set; }
+    public int ConditionRatingNumber { get; set; }
+    public string ConditionRatingNumberDescription { get; set; } = string.Empty;
+}
