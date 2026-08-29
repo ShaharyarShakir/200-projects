6e15926df6910e61cb6bd4bc1fc5a26659ed0ba0
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-29 20:47:52 +0500

    Add migration to seed initial categories in Categories table

---
+++ 06__full-stack/GarageSale/Migrations/20260628101810_SeedCategories.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GarageSale.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CategoryDescription", "CategoryName" },
+                values: new object[,]
+                {
+                    { 1, "Beauty products", "Beauty" },
+                    { 2, "Electronic products", "Electronics" },
+                    { 3, "Furniture products", "Furniture" },
+                    { 4, "Footwear products", "Footwear" },
+                    { 5, "Bedding products", "Bedding" },
+                    { 6, "Bicycle products", "Bicycles" },
+                    { 7, "Camping products", "Camping" },
+                    { 8, "Backpack products", "Backpacks" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
