6aa0a26fef2a0c6d1db255c4a8ff97c4c018afdf
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-29 09:46:35 +0500

    Add CategoryDto class to represent product categories in the application

---
+++ 06__full-stack/GarageSale/DTOs/CategoryDto.cs
@@ -0,0 +1,8 @@
+namespace GarageSale.DTOs;
+
+public class CategoryDto
+{
+    public int Id { get; set; }
+    public string CategoryName { get; set; } = string.Empty;
+    public string? CategoryDescription { get; set; }
+}
