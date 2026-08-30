66c97d6ec0d51cade3ef1dc4fd0ca6f8bae147ba
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-30 21:10:24 +0500

    Add migration to add SalesStatus column to Products table

---
+++ 06__full-stack/GarageSale/Migrations/20260708170105_AddSalesStatusToProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageSale.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSalesStatusToProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SalesStatus",
+                table: "Products",
+                type: "char(1)",
+                nullable: false,
+                defaultValueSql: "'A'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SalesStatus",
+                table: "Products");
+        }
+    }
+}
