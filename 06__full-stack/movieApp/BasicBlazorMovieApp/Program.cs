1f37fbd01a473763b78073206cd2d51fa280cc4e
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-30 22:52:39 +0500

    Add Program.cs for application setup and service configuration

---
+++ 06__full-stack/movieApp/BasicBlazorMovieApp/Program.cs
@@ -0,0 +1,30 @@
+using BasicBlazorMovieApp;
+using BasicBlazorMovieApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<IMovieReviewService, MovieReviewService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
