5721002095fb1a54f2758863fd22425386c7c754
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-08-30 21:07:24 +0500

    Add migration to seed NA condition rating in ConditionRatings table

---
+++ 06__full-stack/GarageSale/Migrations/20260705142039_SeedNAConditionRating.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageSale.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedNAConditionRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ConditionRatings",
+                columns: new[] { "Id", "ConditionRatingNumber", "ConditionRatingNumberDescription" },
+                values: new object[] { 6, 999, "NA" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ConditionRatings",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
