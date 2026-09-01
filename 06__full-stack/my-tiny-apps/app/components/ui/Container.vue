ea71f0bbaa4fa2c899332b2665bfe3904c7d3594
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-09-01 09:45:49 +0500

    feat: add Container component with responsive size classes

---
+++ 06__full-stack/my-tiny-apps/app/components/ui/Container.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  size?: 'sm' | 'md' | 'lg' | 'xl' | 'full'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 'lg',
+})
+
+const sizeClasses = {
+  sm: 'max-w-3xl',
+  md: 'max-w-5xl',
+  lg: 'max-w-6xl',
+  xl: 'max-w-7xl',
+  full: 'max-w-full',
+}
+</script>
+
+<template>
+  <div :class="['mx-auto w-full px-4 sm:px-6 lg:px-8', sizeClasses[props.size]]">
+    <slot />
+  </div>
+</template>
+
+
